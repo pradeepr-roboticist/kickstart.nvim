c09aaa7c3087574080d0b1d659887f4ea8e7458d
Pradeep <pradeepunique1989@gmail.com>
2026-01-08 23:45:36 -0500

    jj for esc

--- init.lua
+++ init.lua
@@ -180,6 +180,8 @@ vim.o.confirm = true
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
+vim.keymap.set('i', 'jj', '<Esc>')
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
