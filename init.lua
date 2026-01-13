3c7f2b1063027cba57c6a11841f9ec108cf186cd
Pradeep Rajendran <pradeepr.roboticist@gmail.com>
2026-01-12 20:48:05 -0500

    Super tab for completion

--- init.lua
+++ init.lua
@@ -877,7 +877,7 @@ require('lazy').setup({
         -- <c-k>: Toggle signature help
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
-        preset = 'enter',
+        preset = 'super-tab',
 
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
