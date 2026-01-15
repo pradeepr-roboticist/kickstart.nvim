2514009691ec643964139c00677c8ae24d3a4db0
Pradeep Rajendran <prajendran@path-robotics.com>
2026-01-14 23:23:35 -0500

    Adding linter

--- init.lua
+++ init.lua
@@ -805,7 +805,7 @@ require('lazy').setup({
         lua = { 'stylua' },
         cpp = { 'clang_format' },
         -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
+        python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
